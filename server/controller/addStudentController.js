7261c81055089ef1cd87ceded4c511719961a887
jshytmpc <joshua.timpac27@gmail.com>
2024-09-04 18:02:12 +0800

    added backend add student

--- server/controller/addStudentController.js
+++ server/controller/addStudentController.js
@@ -21,41 +21,14 @@ export const addStudent = async (req, res) => {
         .json({ success: false, message: "Email already exists" });
     }
 
-    if (existingId) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Id Number already exists" });
-    }
-
-    await newUser.save();
-    generateTokenAndSetCookie(res, newUser._id);
-
     const responseData = {
       success: true,
       message: "Student created successfully",
-      user: { ...newUser._doc },
     };
 
     return res.status(201).json(responseData);
   } catch (error) {
     console.error("Error creating user:", error);
-    return res.status(400).json({ success: false, message: error.message });
-  }
-};
-
-export const checkAuth = async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.userId).select("-password");
-    if (!user) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found" });
-    }
-    return res
-      .status(200)
-      .json({ success: true, user: { ...user._doc, password: undefined } });
-  } catch (error) {
-    console.log("Post request failed", error);
     return res.status(400).json({ success: false, message: error.message });
   }
 };
