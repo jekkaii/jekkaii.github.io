0efd262cac7e99e1c378c7e2d17ed05e20baed95
jshytmpc <joshua.timpac27@gmail.com>
2024-10-24 21:53:26 +0800

    Fix edit profile bug

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -65,11 +65,18 @@ export const editUser = async (req, res) => {
       throw new Error("User not found");
     }
 
+    const checkUserDepartment = await UserModel.findOne({
+      department: department,
+    });
+
     // Update user properties
     user.firstName = firstname;
     user.lastName = lastname;
     user.email = email; // Update email
-    user.department = department;
+
+    if (checkUserDepartment) {
+      user.department = department;
+    }
 
     // Check if a new photo is uploaded and update the photo field
 
@@ -228,8 +235,8 @@ const validateAddUserInput = (req) => {
 
 // validation for editUser
 const validateEditUserInput = (req) => {
-  const { username: id, firstname, lastname, email, role } = req.body;
-  if (!id || !firstname || !lastname || !email || !role) {
+  const { username: id, firstname, lastname, email } = req.body;
+  if (!id || !firstname || !lastname || !email) {
     throw new Error("All fields are required");
   }
 };
